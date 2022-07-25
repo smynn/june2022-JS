d3d7c8552a2a99a5cdedc30c7eaaca73b6d612c1
smynn <ivan.rubak17@gmail.com>
2022-07-25 17:28:40 +0300

    1

---
+++ lessons1/main.js
@@ -0,0 +1,28 @@
+
+
+let he='hello';
+let owu='owu';
+let com='com';
+let ua='ua';
+let nu=1;
+let num=10;
+let neg=-999;
+let numb=123;
+let p=3.14;
+let numbe=2.7;
+let number=16;
+let tr=true;
+let fa=false;
+let firstName=prompt('Your first name?');
+let middleName=prompt('Your middle name?');
+let lastName=prompt('Your last name?');
+let a=100;
+let b='100';
+let c=true;
+
+
+alert(com);
+document.write(`${com} ${ua}`);
+console.log(he, nu , num , neg, owu, numb, p, numbe, number, tr, fa);
+console.log(firstName + ' ' + middleName + ' ' + lastName);
+console.log(typeof a, typeof b, typeof c);
